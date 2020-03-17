27932c7e722edce04a85bcbb7905f9561051a75e
K Nikhil <nikhiltintu7@gmail.com>
2020-03-17 17:08:39 +0530

    Add files via upload

---
+++ example/ambient_test.py
@@ -0,0 +1,26 @@
+from time import sleep
+from machine import Pin, I2C
+from apds9960.const import *
+from apds9960 import uAPDS9960 as APDS9960
+
+bus = I2C(-1, scl = Pin(22), sda = Pin(21))
+
+try:
+  apds = APDS9960(bus)
+except:
+  print("failed to initilize apds, check connection")
+  
+
+apds = APDS9960(bus)
+
+print("Light Sensor Test")
+print("=================")
+apds.enableLightSensor()
+
+oval = -1
+while True:
+    sleep(0.25)
+    val = apds.readAmbientLight()
+    if val != oval:
+        print("AmbientLight={}".format(val))
+        oval = val
